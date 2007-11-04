be424176d30d059c0a60da39a244da4ce614bba2
mike <mike@90940388-7319-0410-b354-a7c10eaab904>
2007-11-04 16:17:32 +0000

    more migration stuff. confused, but whatever.
    git-svn-id: http://buffington.svnrepository.com/svn/llor.nu@4 90940388-7319-0410-b354-a7c10eaab904

--- db/migrate/001_add_instance_id.rb
+++
@@ -1,9 +0,0 @@
-class AddInstanceId < ActiveRecord::Migration
-  def self.up
-    add_column :settings, :instance_id, :integer
-  end
-
-  def self.down
-    remove_column :settings, :instance_id
-  end
-end
